77126ed479bd69189e6bfb0ae70afbdd42ca7604
Ro0t-set <tommaso.patriti@studio.unibo.it>
2022-03-08 15:44:29 +0100

    ble connection refactoring

--- src/ble/bleConnection.py
+++ src/ble/bleConnection.py
@@ -1,27 +1,21 @@
-from email import message
 from bluepy import btle
 import threading
-message = "FiresMessage"
+
+message = ""
+
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
-        # ... initialise here
 
-    def handleNotification(self, cHandle, data):
+    def handle_notification(self, data):
         global message
-        message =  data 
-  
-
-        # ... perhaps check cHandle
-        # ... process 'data'
-
-# Initialisation  -------
+        message = data
 
 
 class BleConnection:
-    def __init__(self, mac_addres, on_message_receive):
-        self.p = btle.Peripheral(mac_addres,btle.ADDR_TYPE_PUBLIC)  
+    def __init__(self, mac_address, on_message_receive):
+        self.p = btle.Peripheral(mac_address, btle.ADDR_TYPE_PUBLIC)
         self.on_message_receive = on_message_receive
         # Setup to turn notifications on, e.g.
         self.svc = self.p.getServiceByUUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
@@ -31,13 +25,10 @@ class BleConnection:
         self.p.setDelegate(MyDelegate())
         self.p.setMTU(512)
         self.setup_data = b"\x01\00"
-        self.p.writeCharacteristic(self.ch_Rx.valHandle+1, self.setup_data)
+        self.p.writeCharacteristic(self.ch_Rx.valHandle + 1, self.setup_data)
 
         self.notification_thread = threading.Thread(target=self.get_notification)
         self.notification_thread.start()
- 
-
-
 
     def get_notification(self):
         global message
@@ -45,14 +36,12 @@ class BleConnection:
             if self.p.waitForNotifications(1.0):
                 self.on_message_receive(message)
 
-        
-    def whrite(self, message):
-            btime = bytes(message , 'utf-8')
-            try:        
-                return self.ch_Tx.write(btime, True)
+    def send_notification(self, message_to_send):
+        str_message_to_bytes = bytes(message_to_send, 'utf-8')
+        try:
+            return self.ch_Tx.write(str_message_to_bytes, True)
 
-            except btle.BTLEException:
-                return "btle.BTLEException"
+        except btle.BTLEException:
+            return "btle.BTLEException"
 
-        
-    # Perhaps do something else here
+
